f6d5476124a8c9614ac9b97b33f8fc5ad78346b9
BruhThisMyAcc <shane.w.brumfield@outlook.com>
2023-12-04 15:51:55 -0600

    Added player rotation
    added player rotation relative to mouse position.

---
+++ SECTion/Assets/Scripts/playerRotation.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRotation : MonoBehaviour{
+    
+    void Update(){
+        //tracks mouse position in the world
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x,
+         mousePosition.y - transform.position.y);
+
+        transform.up = direction;
+    }
+}
