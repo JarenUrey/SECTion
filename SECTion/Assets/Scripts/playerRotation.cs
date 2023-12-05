3ca6c51b4a3a46a7655d65deeefb1b2fc2d4e917
BruhThisMyAcc <shane.w.brumfield@outlook.com>
2023-12-04 20:01:53 -0600

    Camera edits

--- SECTion/Assets/Scripts/playerRotation.cs
+++ SECTion/Assets/Scripts/playerRotation.cs
@@ -9,6 +9,7 @@ public class playerRotation : MonoBehaviour{
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
+        //rotates the player relative to mouse position
         Vector2 direction = new Vector2(mousePosition.x - transform.position.x,
          mousePosition.y - transform.position.y);
 
